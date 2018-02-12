fb51107cba22749facc23c737e4a7420d529a00c
Nikolas Bogucarskis <niktechnopro@gmail.com>
2018-02-11 22:21:27 -0500

    field validation added

--- public/JS/pagescroll.js
+++ public/JS/pagescroll.js
@@ -2,6 +2,9 @@ console.log('loading pagescroll');
 //reading submit button by attribute
 let $submitButton = $('[submitButton]');
 var $form = $('[formReset]')[0];//referencing my table here
+// As per the HTML5 Specification
+// var emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+var emailRegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 // window.onunload = function () {
 //     window.scrollTo(0, 0);
 // }
@@ -53,49 +56,77 @@ $(document).ready(function(){
     $submitButton.on('click', (event)=>{
         event.preventDefault()//
         //reading data from fields
-        name = $('[name = "name"]').val().trim()
-        email = $('[name = "email"]').val().trim()
-        message = $('[name = "textarea"]').val().trim()
+        let name = $('[name = "name"]').val().trim()
+        let email = $('[name = "email"]').val().trim()
+        let message = $('[name = "textarea"]').val().trim()
         //making an object with info
-        data = {
+        let data = {
             name : name,
             email : email,
             message : message
         }
         console.log('reading info from fields', data)
-        //clearing fields
-        // console.log($form)
-        $form.reset()
-        //making ajax post request to route /send
-        $.ajax({
-            url: "/send",
-            method: "POST",
-            data: data,
-            dataType: "JSON"
-            
-        }).done(result => {
-            // console.log("this is done message ", result)
-            if (result.message == 'success'){
-                swal({
-                title: "Thank You For Your Interest! \nI'll get back to you shortly",
-                text: "Press OK button",
-                icon: "success",
+        //checking if email is correct
+        if (name === ""){
+            console.log("the name field is empty")
+            swal({
+                title: "it seems that name field is empty",
+                text: "please type your name",
+                icon: "error",
                 button: "OK!"
                 }).then((result)=>{
-                    console.log('back to about-me section')
-                    // window.location.href = "#about-me";//redirects back to about-me section
-                    //the following will account for header height
-                    let position = $('#about-me').offset().top - headerHeight;
-                    // console.log("navigation clicked", $(linkRef).offset().top, headerHeight, position);
-                    $('html, body').animate({
-                        scrollTop : position
-                        }, 500)
+                    console.log(result)   
+                })
+        }
+        else if (email === "" || !email.match(emailRegExp)){
+            console.log('email is incorrect')
+            // $('[name="email"]').val('please retype correct email').css('color','purple')
+            // $('[name = "email"]').on('focus',function(){
+            //     $(this).val("");
+            // });
+            swal({
+                title: "it seems that something's wrong with email",
+                text: "please check and retype your email",
+                icon: "error",
+                button: "OK!"
+                }).then((result)=>{
+                    console.log(result)   
+                })
+        }else{
+            console.log('email is correct')
+            //clearing fields
+            // console.log($form)
+            $form.reset()
+            //making ajax post request to route /send
+                $.ajax({
+                    url: "/send",
+                    method: "POST",
+                    data: data,
+                    dataType: "JSON"    
+                }).done(result => {
+                    // console.log("this is done message ", result)
+                    if (result.message == 'success'){
+                        swal({
+                        title: "Thank You For Your Interest! \nI'll get back to you shortly",
+                        text: "Press OK button",
+                        icon: "success",
+                        button: "OK!"
+                        }).then((result)=>{
+                            console.log('back to about-me section')
+                            // window.location.href = "#about-me";//redirects back to about-me section
+                            //the following will account for header height
+                            let position = $('#about-me').offset().top - headerHeight;
+                            // console.log("navigation clicked", $(linkRef).offset().top, headerHeight, position);
+                            $('html, body').animate({
+                                scrollTop : position
+                                }, 500)
+                        })
+                    }
+                })
+                .fail(error=>{
+                    console.log('error ocured: ', error)
                 })
             }
-        })
-        .fail(error=>{
-            console.log('error ocured: ', error)
-        })
     })
 
 });
