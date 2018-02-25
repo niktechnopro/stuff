58aec3baeeffbe10dca89c43aed08d7b1f465282
Nikolas Bogucarskis <niktechnopro@gmail.com>
2018-02-25 16:14:03 -0500

    improvements

--- public/JS/googleMap.js
+++ public/JS/googleMap.js
@@ -2,16 +2,34 @@ console.log('loading google map');
   ///////////////////////////////
   // google map
   ///////////////////////////////
+var LatLng = {lat: 33.7537, lng: -84.3850};
+
+// infor about marker with resizing
+var image = {
+    url: "images/mylocation.gif", // url
+    scaledSize: new google.maps.Size(50, 50), // scaled size
+    // origin: new google.maps.Point(0,0), // origin
+    anchor: new google.maps.Point(30, 65) // anchor
+};
+// var image = 'https://i.imgur.com/FRSwoOV.gif';
 
 var mapOptions = {
-  center:new google.maps.LatLng(33.7537,-84.3850),
+  center:new google.maps.LatLng(LatLng),
   zoom:6,
   mapTypeId:google.maps.MapTypeId.ROADMAP,
   disableDefaultUI: true,
   scrollwheel: false
-  };
+
+};
 var map=new google.maps.Map(document.getElementById("googleMap")
   ,mapOptions);
+
+var marker = new google.maps.Marker({
+        position: LatLng,
+        map: map,
+        title: 'I am here!',
+        icon: image
+      });
 
 //to recenter map when window resizing
 google.maps.event.addDomListener(window, 'resize', function() {
