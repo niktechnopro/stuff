2e76743d5df8f9ae5d4ac8333d74f9e0d1716196
Nikolas Bogucarskis <niktechnopro@gmail.com>
2018-02-25 16:36:48 -0500

    fixes

--- public/JS/carousel.js
+++ public/JS/carousel.js
@@ -6,28 +6,6 @@ function autoSpin(){
 	currentSlide = parseInt(current);
 	(currentSlide === 4) ? currentSlide=1 : currentSlide += 1;
 	$(".carousel").attr("slide-number", `${currentSlide}`);
-	// console.log(currentSlide);
 }
 
 const timer = setInterval(autoSpin, 3000);
-
-
-
-// $('.next').click(function(){
-// 	clearInterval(timer);
-// 	var current = $(".carousel").attr("slide-number");
-// 	currentSlide = parseInt(current);
-// 	(currentSlide === 4) ? currentSlide=1 : currentSlide += 1;
-// 	$(".carousel").attr("slide-number", `${currentSlide}`);
-// 	console.log(currentSlide);
-// })
-
-
-// $('.prev').click(function(){
-// 	clearInterval(timer);
-// 	var current = $(".carousel").attr("slide-number")
-// 	currentSlide = parseInt(current);
-// 	(currentSlide === 1) ? currentSlide = 4: currentSlide -= 1;
-// 	$(".carousel").attr("slide-number", `${currentSlide}`);
-// 	console.log(currentSlide);
-// })
